a2c5719450240476c2ab9cebcd76eee89b7af8e1
Martijn Dwars <ikben@martijndwars.nl>
2015-04-19 13:51:38 +0200

    Add backpressure

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -15,11 +15,19 @@ object Main {
     val ssc = new StreamingContext(sparkConf, Seconds(1))
 
     // Use local observable as input stream for Spark
-    val clock = Observable.interval(100 milliseconds)
-    val stream = RxUtils.createStream(ssc, clock)
+    //val nonBackpressureableClock = Observable.interval(100 milliseconds)
+    //val stream = RxUtils.createStream(ssc, nonBackpressureableClock)
+
+    val backpressureableClock = Observable.from(0 to 100)
+    val stream = RxUtils.createBackpressuredStream(ssc, backpressureableClock)
+
+    // Simulate a slow stream so jobs will start piling up
+    val slowStream = stream
+      .map(_ + 1)
+    //.map(_ => Thread.sleep(3000))
 
     // Use output stream from Spark as observable
-    stream
+    slowStream
       .toObservable
       .subscribe(l => println("Observable says: " + l))
     
