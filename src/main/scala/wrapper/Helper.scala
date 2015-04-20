484e725068b2735da5d2310fd49789d1a1de5151
Martijn Dwars <ikben@martijndwars.nl>
2015-04-20 12:39:42 +0200

    Remove redundant server

--- src/main/scala/wrapper/Helper.scala
+++ src/main/scala/wrapper/Helper.scala
@@ -1,10 +1,5 @@
 package wrapper
 
-import java.io.PrintStream
-import java.net.ServerSocket
-
-import org.apache.spark.storage.StorageLevel
-import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.dstream.DStream
 import rx.lang.scala.Observable
 
@@ -14,26 +9,6 @@ object Helper {
     def toObservable = Observable[T](subscriber =>
       stream.foreachRDD(_.collect().map(subscriber.onNext))
     )
-  }
-
-  implicit class ObservableWrapper(obs: Observable[Long]) {
-    def toDStream(ssc: StreamingContext) = {
-      new Thread() {
-        override def run: Unit = {
-
-          val server = new ServerSocket(9999)
-          val s = server.accept()
-          println("Client connected")
-          val out = new PrintStream(s.getOutputStream)
-
-          obs.subscribe(x => out.println(x))
-          while (true) {}
-        }
-      }.start
-
-
-      ssc.socketTextStream("localhost", 9999, StorageLevel.MEMORY_AND_DISK_SER)
-    }
   }
 
 }
